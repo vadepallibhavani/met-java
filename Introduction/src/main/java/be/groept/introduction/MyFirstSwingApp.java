053ed4658eee5ced3198bc74f42d5dd8e0828080
groept <koen.serneel@groept.be>
2012-09-10 13:31:20 +0200

    Adding space

--- Introduction/src/main/java/be/groept/introduction/MyFirstSwingApp.java
+++ Introduction/src/main/java/be/groept/introduction/MyFirstSwingApp.java
@@ -35,7 +35,7 @@ public class MyFirstSwingApp {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
+		frame =  new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
